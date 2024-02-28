e4c96e7d52c1b0849259d8874e534d31f9999ddf
Sparsh Mathur <66290822+sparsh-mathur@users.noreply.github.com>
2024-02-28 21:15:04 +0530

    Time: 62 ms (67.84%) | Memory: 53.3 MB (56.14%) - LeetSync

---
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function(root) {
+    const dq = [root]
+    while(dq.length){
+        const node = dq.shift()
+        if (node.right){
+            dq.push(node.right)
+        }
+        if (node.left){
+            dq.push(node.left)
+        }
+        if (!dq.length){
+            return node.val
+        }
+    }
+    
+};
