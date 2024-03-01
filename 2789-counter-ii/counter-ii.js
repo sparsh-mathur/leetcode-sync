f62580d36fa524b6f4cbd1bfb90142134f9fcb5f
Sparsh Mathur <66290822+sparsh-mathur@users.noreply.github.com>
2024-03-01 08:54:55 +0530

    Time: 64 ms (31.48%) | Memory: 51.9 MB (17.05%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let counter=init;
+    function increment(){
+        counter++;
+        return counter;
+        
+    }
+    function decrement(){
+        counter--;
+        return counter
+
+    }
+    function reset(){
+        counter=init
+        return counter
+    }
+    return {increment,decrement,reset}
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
